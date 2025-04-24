3daac1291b4eab2eb6be7220d7eaa64b42189e84
Shish <shishfruitwala29@gmail.com>
2025-04-24 16:27:41 +0530

    add header and hero section

---
+++ animations.js
@@ -0,0 +1,37 @@
+let menu = document.querySelector(".header-right-hamburger");
+let cross = document.querySelector(".cross");
+let navigator = document.querySelector("div.navigator");
+
+gsap.set(navigator, {
+  opacity: 0,
+  x: 500,
+});
+
+menu.addEventListener("click", () => {
+  gsap.set(navigator, { display: "block" });
+  gsap.to(navigator, {
+    opacity: 1,
+    x: 0,
+    duration: 0.5,
+    ease: "circ.out",
+  });
+});
+
+cross.addEventListener("click", () => {
+  gsap.to(navigator, {
+    x: -500,
+    opacity: 0,
+  });
+
+  setTimeout(() => {
+    gsap.set(navigator, { display: "none", x: 500 });
+  }, 500);
+});
+
+document.body.addEventListener("mousemove", (e) => {
+  gsap.to("#cursor", {
+    x: e.clientX,
+    y: e.clientY,
+    ease: "slow",
+  });
+});
