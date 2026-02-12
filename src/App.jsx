b1a20d00e41d4c1e0015abae4292b43467b001ae
Shish <shishfruitwala29@gmail.com>
2026-02-12 19:04:26 +0530

    delete previous files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { HashRouter } from "react-router-dom"; // Changed from BrowserRouter to HashRouter
 import AppRoutes from "./AppRoutes";
-import MainLayout from "./layouts/MainLayout";
-import CustomCursor from "./components/CustomCursor";
-import { ThemeColorProvider } from "./components/ThemeColorContext";
 
 const App = () => {
   return (
     <div>
-      <ThemeColorProvider>
-        <HashRouter>
-          <CustomCursor />
-          <MainLayout>
-            <AppRoutes />
-          </MainLayout>
-        </HashRouter>
-      </ThemeColorProvider>
+      <AppRoutes />
     </div>
   );
 };
--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,25 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Silverzone from "./pages/Silverzone";
-import HackerRankCSS from "./pages/Hackerrank-css-basic";
-import BBPSHackathon from "./pages/BBPS-hackathon-1-0";
-import ResponsiveWebDesign from "./pages/Responsive-web-design";
-import StationXCertification from "./pages/StationX-certification";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/silverzone" element={<Silverzone />} />
-      <Route path="/hackerrank-css-basic" element={<HackerRankCSS />} />
-      <Route path="/bbps-hackathon-1.0" element={<BBPSHackathon />} />
-      <Route path="/responsive-web-design" element={<ResponsiveWebDesign />} />
-      <Route
-        path="/stationX-certification"
-        element={<StationXCertification />}
-      />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
